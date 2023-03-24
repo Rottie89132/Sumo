aab12ddbc404fb95af440e32b5e2ecf35d5a6183
Rottie#7658 <89132@glr.nl>
2023-03-24 13:15:58 +0100

    verwijderen en toevoegen toegevoegd

--- scripts/client/index.js
+++ scripts/client/index.js
@@ -252,26 +252,4 @@ function categorie(naam) {
       </div>`
     });
   }
-  window.jsPDF = window.jspdf.jsPDF;
-
-  function Convert_HTML_To_PDF() {
-    var doc = new jsPDF();
-    
   
-    // Source HTMLElement or a string containing HTML.
-    var elementHTML = document.querySelector("#producten");
-
-    doc.html(elementHTML, {
-        callback: function(doc) {
-            // Save the PDF
-            doc.save('document-html.pdf');
-        },
-        margin: [10, 10, 10, 10],
-        autoPaging: 'text',
-        x: 0,
-        y: 0,
-        width: 190, //target width in the PDF document
-        windowWidth: 675 //window width in CSS pixels
-    });
-}
-
--- scripts/client/winkelwagen.js
+++ scripts/client/winkelwagen.js
@@ -1,3 +1,140 @@
+let productenLijst = [
+    {
+      naam: "CRISPY CHICKEN SALAD",
+      beschrijving: "KROKANTE KIP | SLA MIX | FRUITY MAYO | SESAM",
+      prijs: 6.0,
+      porties: 2,
+      image: "crisy.png",
+      categorie: "salad",
+    },
+    {
+      naam: "EBIKO SALAD",
+      beschrijving: "KRAB | SLA MIX | MAYO | EBIKO | SESAM",
+      prijs: 5.8,
+      porties: 2,
+      image: "ham.png",
+      categorie: "salad",
+    },
+    {
+      naam: "WAKAME SALAD",
+      beschrijving: "WAKAME | SLA MIX | SESAM SOJA DRESSING | SESAM",
+      prijs: 6.0,
+      porties: 2,
+      image: "green.png",
+      categorie: "salad",
+    },
+    {
+      naam: "SALMON SALAD",
+      beschrijving: "ZALM | SPICY RETTICH | SLA MIX | SESAM SOJA DRESSING",
+      prijs: 7.0,
+      porties: 2,
+      image: "salmon.png",
+      categorie: "salad",
+    },
+    {
+      naam: "EBI CALIFORNIA",
+      beschrijving: "GARNAAL CALIFORNIA",
+      prijs: 11.0,
+      porties: 1,
+      image: "Sushi1.png",
+      categorie: "maki",
+    },
+    {
+      naam: "UNAGI TAMAGO",
+      beschrijving: "GEGRILDE PALING OMELET",
+      prijs: 12.5,
+      porties: 1,
+      image: "Sushi2.png",
+      categorie: "maki",
+    },
+    {
+      naam: "CRISPY MAYO",
+      beschrijving: "KROKANTE MAYO",
+      prijs: 9.0,
+      porties: 1,
+      image: "Sushi3.png",
+      categorie: "maki",
+    },
+    {
+      naam: "TUNA",
+      beschrijving: "TONIJN",
+      prijs: 6.0,
+      porties: 1,
+      image: "Sushi4.png",
+      categorie: "maki",
+    },
+    {
+      naam: "BEEF NOODLES/RICE",
+      beschrijving:
+        "BEEF BLOKJES | TERIYAKISAUS | GEBAKKEN NOEDELS/RIJST OF WITTE RIJST",
+      prijs: 7.0,
+      porties: 2,
+      image: "chick.png",
+      categorie: "Hot bowls",
+    },
+    {
+      naam: "FRIED RICE",
+      beschrijving: "GEBAKKEN RIJST, SESAM, BOSUI",
+      prijs: 7.0,
+      porties: 2,
+      image: "rice.png",
+      categorie: "Hot bowls",
+    },
+    {
+      naam: "VEGGIE NOODLES",
+      beschrijving: "GEBAKKEN NOODLES",
+      prijs: 7.0,
+      porties: 2,
+      image: "vegi.png",
+      categorie: "Hot bowls",
+    },
+    {
+      naam: "SALMON NOODLES/RICE",
+      beschrijving:
+        "GEGRILDE ZALM | PONZU SOYA DRESSING | GEBAKKEN NOEDELS/RIJST OF WITTE RIJST",
+      prijs: 7.0,
+      porties: 2,
+      image: "slamon2.png",
+      categorie: "Hot bowls",
+    },
+    {
+      naam: "MISO SOUP",
+      beschrijving:
+        "MISO SOEP | TOFU | SHIITAKE | BOSUI",
+      prijs: 3.5,
+      porties: 1,
+      image: "Miso.png",
+      categorie: "sides",
+    },
+    {
+      naam: "GYOZA",
+      beschrijving:
+        "6 X GEBAKKEN KIPPASTEI | PONZU SOJA",
+      prijs: 6.8,
+      porties: 1,
+      image: "gyo.png",
+      categorie: "sides",
+    },
+    {
+      naam: "CHICKEN TERRIYAKI",
+      beschrijving:
+        "GEGRILDE KIPPENDIJ | TERIYAKISAUS| SESAM",
+      prijs: 7.0,
+      porties: 1,
+      image: "TERIYAKI.png",
+      categorie: "sides",
+    },
+    {
+      naam: "SPRING ROLLS",
+      beschrijving:
+        "10 X VEGGIE MINI LOEMPIA'S MET SWEET CHILI SAUS",
+      prijs: 6.2,
+      porties: 2,
+      image: "loempie.png",
+      categorie: "sides",
+    },
+  ];
+
 function updateCart() {
     let cart = JSON.parse(sessionStorage.getItem('cart'))
     if(!cart) return;
@@ -9,6 +146,34 @@ function updateCart() {
     let cartBody = document.getElementById('cart')
     cartBody.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> €${prijs.toFixed(2)}`
 }
+
+function addToCart(naam) {
+    let product = productenLijst.find(item => item.naam === naam);
+    let cart = JSON.parse(sessionStorage.getItem('cart')) || []
+    let huidigProduct = cart.find(item => item.naam == naam)
+
+    huidigProduct.aantal++
+    huidigProduct.prijs += product.prijs;
+    sessionStorage.setItem('cart', JSON.stringify(cart))
+    
+    console.log(huidigProduct)
+
+    
+}
+
+function RemoveFromCart(naam) {
+    let product = productenLijst.find(item => item.naam === naam);
+    let cart = JSON.parse(sessionStorage.getItem('cart')) || []
+    let huidigProduct = cart.find(item => item.naam == naam)
+
+    huidigProduct.aantal--
+    huidigProduct.prijs -= product.prijs;
+    sessionStorage.setItem('cart', JSON.stringify(cart))
+    
+    console.log(huidigProduct)
+    
+}
+
 
 function winkelwagen() {
     let winkelwagenBody = document.getElementById('winkelwagenBody')
@@ -37,7 +202,7 @@ function winkelwagen() {
             <td>${product.naam}</td>
             <td>$${product.prijs.toFixed(2)}</td>
             <td>${product.aantal}</td>
-            <td>+ / -</td>
+            <td><button class="btn btn" onclick="addToCart('${product.naam}')"><i class="fa-solid fa-plus"></i></button><button class="btn" onclick="RemoveFromCart('${product.naam}')"><i class="fa-solid fa-trash-can"></i></button></td>
         </tr>`
     });
 
